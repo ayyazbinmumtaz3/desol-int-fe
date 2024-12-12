df800a74cab6823126c3147baf3bb0b1a3d3e5d4
Ayyaz Bin Mumtaz <ayyazbinmumtaz3@gmail.com>
2024-12-12 14:49:23 +0500

    inital commit

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.BACKEND_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
