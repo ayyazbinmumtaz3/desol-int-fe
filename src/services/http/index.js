54f0ade5c63a5d8f62b3bc54489b8e479faae494
Ayyaz Bin Mumtaz <ayyazbinmumtaz3@gmail.com>
2024-12-12 15:48:53 +0500

    inital commit

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: 'https://desol-int-be.vercel.app',
-  timeout: 10000,
+  timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
   },
